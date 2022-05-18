7a4db39d0b50fb9ed3707f10fa8e3d09a1844f8b
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2022-05-18 15:38:59 +0200

    add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+class Main {
+  public String getMessage(){
+    return "Hello World.";
+  }
+
+  public static void main(String[] args) {
+    Main app = new Main();
+    System.out.println(app.getMessage());
+  }
+
+}
